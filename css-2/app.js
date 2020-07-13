2b2d7db34f7313dd428e553b68e2ebd7a94742ae
Jabid <jabid9731@yahoo.com>
2020-07-13 07:32:46 +0600

    css-2 login and registration file added

---
+++ css-2/app.js
@@ -0,0 +1,17 @@
+let x = document.getElementById('login');
+let y = document.getElementById('registration');
+let z = document.getElementById('btn');
+
+
+function registration() {
+
+    x.style.left = '-45rem'
+    y.style.left = '8rem'
+    z.style.left = '12.5rem'
+}
+
+function login() {
+    x.style.left = '8rem'
+    y.style.left = '45rem'
+    z.style.left = '0rem'
+}
